e0b02f862e4a4543825964da90a26ce7bbfcf826
Arnel Forbes <forbesarnel09@gmail.com>
2019-08-30 20:10:54 +0800

    finishing touches for structure

---
+++ src/views/404.vue
@@ -0,0 +1,13 @@
+<template>
+  <message>// 404</message>
+</template>
+
+<script>
+export default {
+  components: {
+    message: () => import('./../components/Message.vue')
+  }
+}
+</script>
+
+<style></style>
